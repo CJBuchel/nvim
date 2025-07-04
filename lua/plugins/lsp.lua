2ee0be9fb0736c6fc95323070a4557fa0f5b303e
CJBuchel <connorbuchel@gmail.com>
2025-07-04 18:47:51 +0800

    Added cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,22 +2,27 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = {
-        "lua_ls",
-        "rust_analyzer",
-      }
+      auto_install = true,
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
-      { 
+      {
         "neovim/nvim-lspconfig",
         opts = {},
         config = function()
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
+          local capabilities = require("cmp_nvim_lsp").default_capabilities()
+          local lspconfig = require("lspconfig")
+
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities
+          })
+
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+          vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+          vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
       },
     },
-  }
+  },
 }
